18b17f28c4c93b3fd9bd4bbfcebc82fdb9eb0da3
Egor283 <Egor.meshkov.2010@mail.ru>
2026-02-25 15:35:37 +0300

    not jump

--- player.gd
+++ player.gd
@@ -7,11 +7,6 @@ const JUMP_VELOCITY = -400.0
 @onready var anim = get_node("AnimatedSprite2D")
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
