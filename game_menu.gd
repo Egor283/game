2d976bfeaa1dce2f27963c74ed4fe6a00f95b7ee
qZenon <Krisp.i00@mail.ru>
2026-02-25 22:57:55 +0300

    lol

--- game_menu.gd
+++ game_menu.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,4 +12,8 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://game_menu.tscn")
+	get_tree().change_scene_to_file("res://menu.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn") # Replace with function body.
